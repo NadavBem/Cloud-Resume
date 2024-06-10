c1e5e5fc45b9f4cf68b9c4e08afe2053e1664f84
NadavBem <nadav0176@gmail.com>
2024-06-10 16:25:18 +0300

    update index.html

--- backend/tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
+++ backend/tests/obj/Debug/net6.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cf566d08dbd0423335db301e268a7e4d5db7fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74995edc0dbd83ef3c309e6140dc7be6b2828546")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
